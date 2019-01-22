b222c5374a6c57505bd69b4f30c5f258b10b0a7d
SImple PHyC <44836193+Linqiaosong@users.noreply.github.com>
2019-01-23 02:09:13 +0800

    Update and rename 求导.cpp to Derivative.cpp

--- Derivative.cpp
+++ Derivative.cpp
@@ -5,9 +5,9 @@ int main()
 {
     double a,d,backward,Forwa_Rd,result;
     cout<<"Input a\n";
-    cin>>a;//求导的点
+    cin>>a;
     cout<<"Input d\n";
-    cin>>d;//精度值
+    cin>>d;
     double i=a+d;//x+dx
     Forwa_Rd=i*i;//f(x+dx)
     backward=a*a;//f(x)
